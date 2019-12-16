8754c92fc3c0d9765d5b9663c13dfbc630738507
ribeiroyoann <yoribeir@student.42.fr>
2019-12-16 22:35:44 +0100

    navigation bar

---
+++ src/containers/HeaderNav/HeaderNav.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Image, Menu, Form, Input, Icon } from "semantic-ui-react";
+import "./HeaderNav.scss";
+import logo from "../../assets/images/logo.jpg";
+
+export class HeaderNav extends React.Component {
+  render() {
+    return (
+      <Menu borderless className="top-menu" fixed="top">
+        <Menu.Item header className="logo">
+          <Image src={logo} size="tiny" />
+        </Menu.Item>
+        <Menu.Menu className="nav-container">
+          <Menu.Item className="search-input">
+            <Form>
+              <Form.Field>
+                <Input placeholder="Search" size="small" action="Go" />
+              </Form.Field>
+            </Form>
+          </Menu.Item>
+          <Menu.Menu position="right">
+            <Menu.Item>
+              <Icon
+                className="header-icon"
+                name="video camera"
+                size="large"
+              ></Icon>
+            </Menu.Item>
+            <Menu.Item>
+              <Icon
+                className="header-icon"
+                name="grid layout"
+                size="large"
+              ></Icon>
+            </Menu.Item>
+            <Menu.Item>
+              <Icon className="header-icon" name="chat" size="large"></Icon>
+            </Menu.Item>
+            <Menu.Item>
+              <Icon className="header-icon" name="alarm" size="large"></Icon>
+            </Menu.Item>
+            <Menu.Item>
+              <Image src="http://via.placeholder.com/80x80" avatar />
+            </Menu.Item>
+          </Menu.Menu>
+        </Menu.Menu>
+      </Menu>
+    );
+  }
+}
+
+export default HeaderNav;
