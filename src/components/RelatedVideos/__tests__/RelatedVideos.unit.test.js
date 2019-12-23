7c4a2c0fe9c2ce4ba1cb966fd4b08c711cfbd516
ribeiroyoann <yoribeir@student.42.fr>
2019-12-23 19:07:23 +0100

    related video component

---
+++ src/components/RelatedVideos/__tests__/RelatedVideos.unit.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { RelatedVideos } from "../RelatedVideos";
+
+test("renders RelatedVideos", () => {
+  const wrapper = shallow(<RelatedVideos />);
+  expect(wrapper).toMatchSnapshot();
+});
