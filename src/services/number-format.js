254498959f2d48d7dc3b06b14566795ff0e24360
ribeiroyoann <yoribeir@student.42.fr>
2020-01-21 16:48:09 +0100

    api + redux + sagas

---
+++ src/services/number-format.js
@@ -0,0 +1,19 @@
+// https://stackoverflow.com/questions/2685911
+export function abbrNum(number, decPlaces) {
+  decPlaces = Math.pow(10, decPlaces);
+  var abbrev = ["k", "m", "md", "t"];
+
+  for (var i = abbrev.length - 1; i >= 0; i--) {
+    var size = Math.pow(10, (i + 1) * 3);
+    if (size <= number) {
+      number = Math.round((number * decPlaces) / size) / decPlaces;
+      if (number === 1000 && i < abbrev.length - 1) {
+        number = 1;
+        i++;
+      }
+      number += abbrev[i];
+      break;
+    }
+  }
+  return number;
+}
